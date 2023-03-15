3a706d950ce05e58f3cb46ea4571e66103438580
rummanH <jah.hasant@gmail.com>
2023-03-15 19:52:08 +0600

    complete design

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import InfoOutlined from "@mui/icons-material/InfoOutlined";
 import PlayArrow from "@mui/icons-material/PlayArrow";
+import img from "./back.jpg"
 
 import "./featured.scss";
 
@@ -27,10 +28,10 @@ export default function Featured({ type }) {
           </select>
         </div>
       )}
-      {/* <img
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+      <img
+        src={img}
         alt=""
-      /> */}
+      />
       <div className="info">
         <img
           src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Featured from "../components/featured/Featured";
-import List from "../components/list/List";
-import Navbar from "../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
+import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
@@ -9,8 +9,8 @@ const Home = () => {
     <div className="home">
       <Navbar />
       <Featured type="movie" />
-      <List/>
-      <List/>
+      <List />
+      <List />
     </div>
   );
 };
